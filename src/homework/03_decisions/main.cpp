f69a5c46fc2e1030c18147663446228f725fc4e0
chaunskey <chancekidd@gmail.com>
2022-09-18 23:05:56 -0500

    Homework 3 - fixed the logic so program exits if user doesn't input 1 or 2 to continue

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -12,21 +12,22 @@ int main()
                  "2-Letter grade using switch\n"
                  "3-Exit\n";
     std::cin >> choice;
-    std::cout << "Please enter number grade: ";
-    std::cin >> grade;
-    if (grade < 0 or grade > 100) {
-        std::cout << "What? That's not a real grade. "
-                     "Real grades are between 0 and 100!";
-    }
-    else {
-        grade = grade/10;
-        switch (choice) {
-            case 1:
-                std::cout << get_letter_grade_using_if(grade);
-                break;
-            case 2:
-                std:: cout << get_letter_grade_using_switch(grade);
-                break;
+    if (choice == 1 or choice == 2) {
+        std::cout << "Please enter number grade: ";
+        std::cin >> grade;
+        if (grade < 0 or grade > 100) {
+            std::cout << "What? That's not a real grade. "
+                         "Real grades are between 0 and 100!";
+        } else {
+            grade = grade / 10;
+            switch (choice) {
+                case 1:
+                    std::cout << get_letter_grade_using_if(grade);
+                    break;
+                case 2:
+                    std::cout << get_letter_grade_using_switch(grade);
+                    break;
+            }
         }
     }
 	return 0;
