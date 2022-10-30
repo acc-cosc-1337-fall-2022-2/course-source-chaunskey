7d1820f03ca0741ee7e88e94bc2d7b69b32c6430
chaunskey <chancekidd@gmail.com>
2022-10-29 19:07:21 -0500

    added code in test case so it properly terminates.

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -7,14 +7,15 @@ TEST_CASE("Verify Test Configuration", "verification") {
 }
 
 TEST_CASE("Test game over if >9 slots selected") {
+    vector<int> tester{1,2,3,4,5,7,6,9,8};
     TicTacToe game;
     game.start_game("X");
-    vector<int> tester{1,2,3,4,5,7,6,9,8};
     for (int i = 0; i<8; i++) {
         game.mark_board(tester[i]-1);
         REQUIRE(game.game_over() == false);
     }
     game.mark_board(tester[8]-1);
     REQUIRE(game.game_over() == true);
+    string play_again = "N";
 }
 
